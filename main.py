a574733a03deb1a6d9e5fea6fef2143d1ca760c6
Weldaline Zafira W <32257034+weldalinez@users.noreply.github.com>
2019-05-22 16:04:49 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import processes as coba
+
+n = 2
+w = 2
+p = 2
+student = 1
+question = 5
+human_rater = [35, 73, 36, 62, 42, 75, 90, 65, 86, 68, 97, 57, 15, 77, 92, 97, 93, 96, 80, 87, 88, 98, 94, 96, 98, 79,
+               79, 81, 70, 74, 37, 81, 37, 77, 90, 73, 42, 96, 87, 94, 88, 63, 72]
+
+# jumlah mahasiswa
+for count in range(1, student + 1):
+    print('===========================================================================================================')
+    print("MAHASISWA", count)
+    # jumlah soal
+    arr_score = []
+    temp_score = []
+    doc = coba.read_txt("mahasiswa" + str(count) + ".docx")
+    print(doc)
+    for q in range(0, question):
+        # process the student's answer document
+        print('-------------------------------------------------------')
+        print('Jawaban', q+1)
+        prep = coba.preprocessing(doc[q])
+        ngramprep = coba.nGram(prep)
+        print(prep)
+        print(ngramprep, '\n')
+        current_score = 0
+        score = 0
+        key = coba.read_txt("jwbDosen"+str(q+1)+".docx")
+        # pemrosesan untuk tiap kunci
+        kj=1
+        for x in range(0, len(key)):
+            print('Kunci Jawaban '+str(kj))
+            # key = coba.read_txt("key" + str(q) + "-" + str(x) + ".docx")
+            prep2 = coba.preprocessing(key[x])
+            ngramprep2 = coba.nGram(prep2)
+            print(prep2)
+            print(ngramprep2)
+            kj+=1
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#comment dulu
+            # winnowing = coba.winnow(prep, p, n, w)
+            # winnowing2 = coba.winnow(prep2, p, n, w)
+            # # similarity measurement
+            # jac_measure = coba.jaccard(winnowing, winnowing2)
+            # dice_measure = coba.dice(winnowing, winnowing2)
+            # cos_measure = coba.cosine(winnowing, winnowing2)
+            # # print("jaccard : " + str(jac_measure) + " | dice : " + str(dice_measure) + " | cosine : " + str(cos_measure))
+            # # nilai terbesar dari ketiga metode pengukuran
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# comment dulu
+#             score = max(jac_measure, dice_measure, cos_measure)
+#             temp_score = score
+#             if current_score <= temp_score:
+#                 current_score = temp_score
+#             else:
+#                 continue
+#---------------------------------------------------------------------------------------------------------------------------------------------
+#dicomment dulu
+    #     print("\nNilai Nomor " + str(q+1) + " : " + str(current_score))
+    #     # print(current_score)
+    #     arr_score.append(current_score)
+    # print('-------------------------------------------------------')
+    # # nilai total
+    # average = sum(arr_score) / len(arr_score)
+    # print("Total Skor     : " + str(average))
+    # # print(average)
+    # # akurasi sistem
+    # acc = 100 - (((abs(average - human_rater[count - 1])) / 100) * 100)
+    # print("Human Rater    : " + str(human_rater[count - 1]))
+    # # print(human_rater[count-1])
+    # print("Akurasi Sistem : " + str(acc))
+    # # print(acc)
---
+++ processes.py
@@ -0,0 +1,199 @@
+from pykakasi import kakasi  # untuk proses mengubah karakter jp ke roman
+import re  # regular expression
+import docx2txt
+import tinysegmenter
+from nltk.util import ngrams
+
+kakasi = kakasi()
+
+
+# read documents, .txt or .docx
+def read_txt(doc):
+    # Convert from Windows-1252 to UTF-8
+    # encoded = doc.encode('utf-8')
+    # return open(encoded).read()
+    # return open(doc, encoding='ISO-8859-1').read() #berhasil tapi ga tepat, hrsnya encoder cp1252
+    # return open(doc, encoding='cp1252').read()
+    tes = docx2txt.process(doc)
+    tes1 = tes.splitlines()  # split document to lists on new line
+    tes1 = [x for x in tes1 if not x.isdigit()]  # remove number from words
+    tes1 = [x for x in tes1 if x]  # remove empty list
+    return tes1
+    # return open(doc).read() #ga berhasil grgr tipe encodingnya
+
+
+'''
+mulai  preprocessing
+1. menghilangkan kata-kata yang mengulang soal
+2. romanisasi, konversi kana ke romaji
+3. whitespace removal, menghapus spasi dan enter
+4. case folding, menyeragamkan ke lower case
+5. filtering, menghapus selain huruf dan angka
+
+'''
+
+
+# menghapus kata yang mengulang soal
+def remove_rep(text):
+    rep_words = ["2020年東京オリンピックのマスコットは", "ハラルは", "ハラルというは", "ハラルというのは",
+                 "生前退位は", "生前退位というは", "生前退位というのは",
+                 "衆院選は", "衆院選というは", "衆院選というのは",
+                 "Uターンは", "Uターンのは", "Uターンというは", "Uターンというのは",
+                 "Uターン就職は", "Uターン就職というは", "U-ターン就職というのは"]
+
+    word_list = set(rep_words)
+    for words in word_list:
+        if words in text:
+            text = text.replace(words, "")
+    return text
+
+
+# mengubah katakana, hiragana, dan kanji ke romaji (romanisasi)
+def to_romaji(text_jpn):
+    segmenter = tinysegmenter.TinySegmenter()
+    text = ' '.join(segmenter.tokenize(text_jpn))
+    kakasi.setMode("H", "a")  # Hiragana ke romaji
+    kakasi.setMode("K", "a")  # Katakana ke romaji
+    kakasi.setMode("J", "a")  # Japanese ke romaji
+    kakasi.setMode("r", "Hepburn")  # default: Hepburn Roman table
+    # kakasi.setMode("s", True) # add space, default: no separator
+    # kakasi.setMode("C", True) # capitalize, default: no capitalize
+    convert = (kakasi.getConverter()).do(text)
+    return convert
+
+
+# filtering
+def filter_text(romaji):
+    filtering = re.sub("\n", "", romaji).casefold()
+    filtering = re.sub("[^A-Za-z0-9]+", " ", filtering)
+    return filtering
+
+
+# selesai preprocessing
+
+
+'''
+mulai winnowing
+1. pembentukan ngram (penggalan lata) sebanyak n
+2. hashing, konversi tiap karakter pada ngram ke bentuk hash dengan rumus rolling hash
+3. windowing, tiap hash yang dibentuk dikelompokkan ke window sepanjang w
+4. fingerprinting, memilih fingerprint terkecil dari tiap window, jika ada nilai yang sama yang diambil yang ada di window paling kanan
+
+'''
+
+
+# n gram
+def nGram(text):
+    # ngram = [text[i:i + n] for i in range(len(text) - n + 1)]
+    tokens = [text for text in text.split(" ") if text != ""]
+    print(tokens)
+    ngramres = list(ngrams(tokens, 5))
+    return ngramres
+
+
+# hashing
+def to_hash(text, p):
+    result = 0
+    length = len(text)
+    ascii_code = [ord(i) for i in text]  # mengubah ke ascii
+    for i in range(length):
+        result = result + (ascii_code[i] * pow(p, length - 1))  # rumus rolling hash
+        length = length - 1
+    return result
+
+
+def hashing(ngram, p):
+    roll_hash = [to_hash(ngram[i], p) for i in range(len(ngram))]
+    return roll_hash
+
+
+# windowing
+def windowing(roll_hash, w):
+    window = [roll_hash[i:i + w] for i in range(len(roll_hash) - w + 1)]
+    return window
+
+
+# fingerprint
+def fingerprint(window, w):
+    fingers = []
+    current_min = None
+    # untuk tiap window
+    for i in range(0, len(window)):
+        minimum = window[i][0]
+        # untuk window sepanjang w
+        for j in range(1, w):
+            if window[i][j] <= minimum:
+                minimum = window[i][j]
+        # menyimpan nilai minimum tiap window ke list
+        if current_min != minimum:
+            fingers.append(minimum)
+        elif minimum == window[i][w - 1]:
+            fingers.append(minimum)
+        current_min = minimum
+    return fingers
+
+
+# selesai winnowing
+
+
+'''
+mulai similarity measurement, mencari tingkat kesamaan fingerprint
+1. jaccard similarity
+2. dice coefficient
+3. cosine similarity
+'''
+
+
+# jaccard similarity
+def jaccard(fingerprint1, fingerprint2):
+    num = len(set(fingerprint1).intersection(set(fingerprint2)))
+    denum = len(set(fingerprint1).union(set(fingerprint2)))
+    if denum == 0:
+        jaccard = 0.0
+    else:
+        jaccard = float(num / denum) * 100
+    return jaccard
+
+
+# dice coefficient
+def dice(fingerprint1, fingerprint2):
+    num = 2 * (len(set(fingerprint1).intersection(set(fingerprint2))))
+    denum = len(set(fingerprint1)) + len(set(fingerprint2))
+    if denum == 0:
+        dice = 0.0
+    else:
+        dice = float(num / denum) * 100
+    return dice
+
+
+# cosine similarity
+def cosine(fingerprint1, fingerprint2):
+    num = len(set(fingerprint1).intersection(set(fingerprint2)))
+    denum = (len(set(fingerprint1)) ** .5) * (len(set(fingerprint2)) ** .5)
+    if denum == 0:
+        cosine = 0.0
+    else:
+        cosine = float(num / denum) * 100
+        if cosine > 100:
+            cosine = 100.0
+    return cosine
+
+
+# selesai similarity measurement
+
+# run preprocessing
+def preprocessing(text):
+    text = remove_rep(text)
+    romaji = to_romaji(text)
+    # print(romaji)
+    filtering = filter_text(romaji)
+    return filtering
+
+
+# run winnowing
+def winnow(text, p, n, w):
+    ngram = nGram(text, n)
+    roll_hash = hashing(ngram, p)
+    window = windowing(roll_hash, w)
+    fingerprinting = fingerprint(window, w)
+    return fingerprinting
